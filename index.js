e6b7f4806f4ecd1ef0bd9624957df881659ee43a
PratyushayanPaul <paulpaku@gmail.com>
2022-07-28 12:36:49 +0530

    Solved

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 class SortedList {
   constructor() {
-  
+    this.items = [];
+    this.length = 0;
   }
-  add() {
+  add(num) {
+    this.items.push(num);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
    
   }
-  get() {
-
+  get(pos) {
+    if(pos <= this.length){
+      return this.items.indexOf(pos);
+    }
+    else{
+      throw new Error('OutOfBounds');
+    }
   }
   max() {
-
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.max(...this.items)
+    }
   }
   min() {
-
-  }
-  avg() {
-    
-   
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items);
+    }
   }
 
   sum() {
-  
+    if(this.length>0){
+      return this.items.reduce((a,b)=>a+b,0);
+    }
+    else{
+      return 0;
+    }
   }
-  
+
+  avg() {
+    if(this.length>0){
+      return this.sum()/this.length;
+    }
+    else{
+      throw new Error("EmptySortedList");
+    }
+  }
 };
 
 module.exports = SortedList;
